3890a3dd1d6d9ff744cf72833213853062ed8903
DevonMashru <DevonMashru21@gmail.com>
2021-03-25 03:31:20 +0000

    03:31

--- Wakanda Sports Testing/tstStockCollection.cs
+++ Wakanda Sports Testing/tstStockCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WakandaSportsClasses;
+using System.Collections.Generic;
 
 namespace Wakanda_Sports_Testing
 {
@@ -12,6 +13,23 @@ namespace Wakanda_Sports_Testing
         {
             clsStockCollection AllStocks = new clsStockCollection();
             Assert.IsNotNull(AllStocks);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.ItemNo = 77;
+            TestItem.Name = "Mercurial Dream Superfly 8";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Category = "Football Boots";
+            TestItem.Brand = "Nike";
+            TestItem.Size = "UK 6 (EU 39)";
+            TestItem.Price = 85;
+            TestItem.SerialNumber = 1367;
+            TestItem.Active = true;
         }
     }
 }
