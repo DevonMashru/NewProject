e2c1284ea043155b2806847f1d7c03c5c4ba2e5d
DevonMashru <DevonMashru21@gmail.com>
2021-03-25 16:10:34 +0000

    16:49

--- Wakanda Sports Testing/tstStockCollection.cs
+++ Wakanda Sports Testing/tstStockCollection.cs
@@ -30,6 +30,9 @@ namespace Wakanda_Sports_Testing
             TestItem.Price = 85;
             TestItem.SerialNumber = 1367;
             TestItem.Active = true;
+            TestList.Add(TestItem);
+            AllStocks.StockList = TestList;
+            Assert.AreEqual(AllStocks.StockList, TestList);
         }
 
         [TestMethod]
@@ -39,6 +42,51 @@ namespace Wakanda_Sports_Testing
             Int32 SomeCount = 0;
             AllStocks.Count = SomeCount;
             Assert.AreEqual(AllStocks.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            clsStock TestItem = new clsStock();
+            TestItem.ItemNo = 77;
+            TestItem.Name = "Mercurial Dream Superfly 8";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Category = "Football Boots";
+            TestItem.Brand = "Nike";
+            TestItem.Size = "UK 6 (EU 39)";
+            TestItem.Price = 85;
+            TestItem.SerialNumber = 1367;
+            TestItem.Active = true;
+            AllStocks.ThisStock = TestItem;
+            Assert.AreEqual(AllStocks.ThisStock, TestItem);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.ItemNo = 77;
+            TestItem.Name = "Mercurial Dream Superfly 8";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Category = "Football Boots";
+            TestItem.Brand = "Nike";
+            TestItem.Size = "UK 6 (EU 39)";
+            TestItem.Price = 85;
+            TestItem.SerialNumber = 1367;
+            TestItem.Active = true;
+            TestList.Add(TestItem);
+            AllStocks.StockList = TestList;
+            Assert.AreEqual(AllStocks.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordPresent()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            Assert.AreEqual(AllStocks.Count, 2);
         }
     }
 }
--- WakandaSportsClasses/clsStockCollection.cs
+++ WakandaSportsClasses/clsStockCollection.cs
@@ -1,7 +1,32 @@
-﻿namespace WakandaSportsClasses
+﻿using System.Collections.Generic;
+
+namespace WakandaSportsClasses
 {
     public class clsStockCollection
     {
-        public int Count { get; set; }
+        List<clsStock> mStockList = new List<clsStock>();
+        public List<clsStock> StockList
+        {
+            get
+            {
+                return mStockList;
+            }
+            set
+            {
+                mStockList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsStock ThisStock { get; set; }
     }
 }
